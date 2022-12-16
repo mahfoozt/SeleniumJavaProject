88742df068034634241dacf4aeadb271675ea523
mahfoozt <101378374+mahfoozt@users.noreply.github.com>
2022-12-15 22:00:28 -0700

    second time commit

---
+++ reports/report.js
@@ -0,0 +1,272 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/features/doc.feature");
+formatter.feature({
+  "name": "Automate TestAUI Application",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "TC001_DragAndDrop",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Launch Chromebrowser and load url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepsDefinitions.launchChromebrowserAndLoadUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click the Droppable button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.clickTheDroppableButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "its navigate the droppable page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepsDefinitions.itsNavigateTheDroppablePage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "switching to frame",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.switchingToFrame()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "drag and drop action code is performing",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.dragAndDropActionCodeIsPerforming()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "switch back to main frame",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.switchBackToMainFrame()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "its navigate the droppable page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepsDefinitions.itsNavigateTheDroppablePage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click the sortable button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.clickTheSortableButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "its navigate the sortable page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepsDefinitions.itsNavigateTheSortablePage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verification of the title page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.verificationOfTheTitlePage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "close browser",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.closeBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC002_Draggable",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Launch Chromebrowser and load url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepsDefinitions.launchChromebrowserAndLoadUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click the draggable button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.clickTheDraggableButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "its navigated the draggable page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepsDefinitions.itsNavigatedTheDraggablePage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "switching to frame",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.switchingToFrame()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "drag and pull action code is performing",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.dragAndPullActionCodeIsPerforming()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "switch back to main frame",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.switchBackToMainFrame()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "close browser",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.closeBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC003_KeyDown",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Launch Chromebrowser and load url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepsDefinitions.launchChromebrowserAndLoadUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click the selectable button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.clickTheSelectableButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "its navigated the selectable page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepsDefinitions.itsNavigatedTheSelectablePage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "switching to frame",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepsDefinitions.switchingToFrame()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click the item numbers",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.clickTheItemNumbers()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "switch back to main frame",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.switchBackToMainFrame()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "close browser",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepsDefinitions.closeBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
